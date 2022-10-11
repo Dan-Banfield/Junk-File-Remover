f4550bf61175d95f2f3b46c294e7a0f47df612d7
Dan Banfield <dandbanfield@gmail.com>
2022-10-11 20:05:08 +0100

    Add application icon.

--- Junk File Remover/Program.cs
+++ Junk File Remover/Program.cs
@@ -27,11 +27,15 @@ namespace Junk_File_Remover
                 ConsoleExtensions.LINE_TYPE.INFO);
             Deletion.DeleteUpdateFiles().Wait();
 
-            ConsoleExtensions.WriteLine("Removing temporary Windows files...",
+            ConsoleExtensions.WriteLine("Removing temporary Windows files...\n\n",
                 ConsoleExtensions.LINE_TYPE.INFO);
             Deletion.DeleteTemporaryWindowsFiles().Wait();
 
+            ConsoleExtensions.WriteLine("Done! Press any key to exit.",
+                ConsoleExtensions.LINE_TYPE.INFO);
+
             Console.ReadKey();
+            Environment.Exit(0);
         }
     }
 }
